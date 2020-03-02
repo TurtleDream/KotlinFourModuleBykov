b4a394791817520535f2b62ff70f31adb054f74e
TurtleDream <stanislav.byckov@gmail.com>
2020-03-02 14:07:26 +0300

    Add Task17

---
+++ src/Task17.kt
@@ -0,0 +1,26 @@
+// Задание 17: Авиакомпания 1
+// Поможем авиакомпании в создании программы для учета авиапарка.
+// Создайте класс Aircraft и реализуйте в нем свойства, содержащие следующее: номер воздушного судна, максимальная дальность полета, вместимость бака, расход топлива на 100 км.
+// Установите значения по своему усмотрению.
+// Переопределите геттер свойства содержащего расход топлива, чтобы он самостоятельно рассчитывался из свойств дальности полета и вместимости бака.
+fun main(){
+    var aircraft: Aircraft = Aircraft("Боинг 737-800", 50000f, 200)
+
+    aircraft.getInfo()
+}
+
+class Aircraft(private val number: String, private val maxFlightDistance: Float, private val tankCapacity: Int) {
+    private val fuelConsumptionPer100Km: Float
+    get() {
+        return (100 * tankCapacity) / maxFlightDistance
+    }
+
+    fun getInfo(){
+        println("""
+            |Номер судна - '$number'
+            |Максимальная длительность полета - $maxFlightDistance км
+            |Вместимость бака - $tankCapacity л
+            |Расход топлива на 100 км - $fuelConsumptionPer100Km л/100км
+        """.trimMargin())
+    }
+}
